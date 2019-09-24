9d6a0f16ecdc4b5b263c6390eacdf540056b81ba
yasser <yasser.info.work@gmail.com>
2019-09-24 22:01:03 +0530

    Added numberOfSubsets!

---
+++ DP/nofsubset.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+//print the distinct number of subsets of a given string length of n
+int main(){
+    int n;
+    cin>>n;
+    cout<<pow(2,n-1)<<"\n";
+    return 0;
+}
