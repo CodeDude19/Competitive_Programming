3316c744a2b591dfea4e00a4e92914c40b17dc4a
yasser <yasser.info.work@gmail.com>
2020-03-28 00:06:14 +0530

    Update

--- Hackerrank/Dynamic_Programming/kadaneAlgo.cpp
+++ Hackerrank/Dynamic_Programming/kadaneAlgo.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, -4};
-    int n = 9;
+    int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+    int n = 8;
     int maxtillnow = INT_MIN, maxendinghere = 0;
     for (int i = 0; i < n; i++)
     {
